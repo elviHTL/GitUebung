a27bfd1b1f3ae0b6685b60ba4b749c31a6b8af12
elviHTL <elvivodopi1@gmail.com>
2026-09-14 10:07:51 +0200

    repo init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = "Elvi";
+        String surname = "Vodopi";
+        System.out.println("Hello " + name + surname);
     }
 }
